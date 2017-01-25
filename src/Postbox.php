df610b1f82db967656f0a35a947274761820e3e1
ueef <ueef@maximail.org>
2017-01-25 16:37:42 +0500

    remove some methods

--- src/Postbox.php
+++ src/Postbox.php
@@ -5,7 +5,6 @@ namespace Ueef\Postbox {
     use Ueef\Postbox\Interfaces\DriverInterface;
     use Ueef\Postbox\Interfaces\PostboxInterface;
     use Ueef\Postbox\Interfaces\EnvelopeInterface;
-    use Ueef\Postbox\Interfaces\ResponseInterface;
     use Ueef\Assignable\Traits\AssignableTrait;
     use Ueef\Assignable\Interfaces\AssignableInterface;
 
@@ -29,26 +28,6 @@ namespace Ueef\Postbox {
             $this->driver->wait($from, function (string $response) use ($handler) {
                 call_user_func($handler, $this->envelope->parseResponse($response));
             });
-        }
-
-        public function send(array $address, array $data)
-        {
-            $request = new Request([
-                'address' => $address,
-                'data' => $data,
-            ]);
-
-            $this->driver->send($request->getService(), $this->envelope->makeRequest($request));
-        }
-
-        public function request(array $address, array $data): ResponseInterface
-        {
-            $request = new Request([
-                'address' => $address,
-                'data' => $data,
-            ]);
-
-            return $this->envelope->parseResponse($this->driver->request($request->getService(), $this->envelope->makeRequest($request)));
         }
     }
 }
