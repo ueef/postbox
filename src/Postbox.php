bcc0366e782dd11aaf78b1ab6a15d3e287c7ec70
ueef <ueef@maximail.org>
2017-01-26 11:09:56 +0500

    Postbox: making response

--- src/Postbox.php
+++ src/Postbox.php
@@ -2,6 +2,8 @@
 
 namespace Ueef\Postbox {
 
+    use Throwable;
+    use Ueef\Postbox\Exceptions\Exception;
     use Ueef\Postbox\Interfaces\DriverInterface;
     use Ueef\Postbox\Interfaces\PostboxInterface;
     use Ueef\Postbox\Interfaces\EnvelopeInterface;
@@ -25,8 +27,28 @@ namespace Ueef\Postbox {
 
         public function wait(string $from, callable $handler)
         {
-            $this->driver->wait($from, function (string $response) use ($handler) {
-                call_user_func($handler, $this->envelope->parseResponse($response));
+            $this->driver->wait($from, function (string $request) use ($handler) {
+                $response = new Response();
+
+                try {
+                    $request = $this->envelope->parseRequest($request);
+                    $response->assign([
+                        'address' => $request->getAddress(),
+                        'data' => call_user_func($handler, $request),
+                    ]);
+                } catch (Throwable $e) {
+                    $errorCode = $e->getCode();
+                    if (Exception::CODE_NONE == $errorCode) {
+                        $errorCode = Exception::CODE_UNKNOWN;
+                    }
+
+                    $response->assign([
+                        'error_code' => $errorCode,
+                        'error_message' => $e->getMessage(),
+                    ]);
+                }
+
+                return $this->envelope->makeResponse($response);
             });
         }
     }
