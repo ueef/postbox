9474bc88e2d4fd6422c3d70908ad49ba5d1481be
ueef <ueef@maximail.org>
2017-01-26 13:10:27 +0500

    Postman fix

--- src/Postman.php
+++ src/Postman.php
@@ -5,6 +5,7 @@ namespace Ueef\Postbox {
     use Ueef\Postbox\Interfaces\DriverInterface;
     use Ueef\Postbox\Interfaces\PostmanInterface;
     use Ueef\Postbox\Interfaces\EnvelopeInterface;
+    use Ueef\Postbox\Interfaces\RequestInterface;
     use Ueef\Postbox\Interfaces\ResponseInterface;
     use Ueef\Assignable\Traits\AssignableTrait;
     use Ueef\Assignable\Interfaces\AssignableInterface;
@@ -26,22 +27,25 @@ namespace Ueef\Postbox {
 
         public function send(array $route, array $data)
         {
-            $request = new Request([
-                'route' => $route,
-                'data' => $data,
-            ]);
+            $request = $this->makeRequest($route, $data);
 
             $this->driver->send($request->getQueue(), $this->envelope->makeRequest($request));
         }
 
         public function request(array $route, array $data): ResponseInterface
         {
-            $request = new Request([
-                'route' => $route,
-                'data' => $data,
-            ]);
+            $request = $this->makeRequest($route, $data);
 
             return $this->envelope->parseResponse($this->driver->request($request->getQueue(), $this->envelope->makeRequest($request)));
+        }
+
+        private function makeRequest(array $route, array $data): RequestInterface
+        {
+            return new Request([
+                'route' => $route,
+                'queue' => reset($route),
+                'data' => $data,
+            ]);
         }
     }
 }
