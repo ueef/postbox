91ebea4d82ccc616b3069b6e59357e79c6a8aa6b
ueef <ueef@maximail.org>
2017-01-27 13:27:15 +0500

    ValidationTrait

---
+++ src/Traits/ValidationTrait.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Postbox\Traits {
+
+    trait ValidationTrait
+    {
+        private function validate(array $data, array $validators)
+        {
+            foreach ($validators as $validator => $fields) {
+                if (!$fields) {
+                    continue;
+                }
+
+                $validator = ucwords(str_replace(['-', '_'], ' ', $validator));
+                $validator = 'validate' . str_replace(' ', '', $validator);
+
+                if (!method_exists($this, $validator)) {
+                    return 'validator "' . $validator . '" does\'nt exists';
+                }
+
+                foreach ($fields as $field) {
+                    $message = $this->{$validator}($field, $data);
+
+                    if ($message) {
+                        return $message;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private function validateRequired(string $field, array $data)
+        {
+            if (!isset($data[$field])) {
+                return '"' . $field . '" is required';
+            }
+
+            return false;
+        }
+
+        private function validateIsArray(string $field, array $data)
+        {
+            if (isset($data[$field]) && !is_array($data[$field])) {
+                return '"' . $field . '" is not an array';
+            }
+
+            return false;
+        }
+
+        private function validateIsInteger(string $field, array $data)
+        {
+            if (isset($data[$field]) && !is_integer($data[$field])) {
+                return '"' . $field . '" is not an integer';
+            }
+
+            return false;
+        }
+    }
+}
