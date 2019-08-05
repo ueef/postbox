6d62c055b844970b14c9af06f8da21f7c8233733
ueef <artem@arkadev.org>
2019-08-05 13:10:34 +0500

    wait can be non blocking 2

--- src/Interfaces/PostboxInterface.php
+++ src/Interfaces/PostboxInterface.php
@@ -5,7 +5,7 @@ namespace Ueef\Postbox\Interfaces;
 
 interface PostboxInterface
 {
-    public function wait();
+    public function wait(bool $nonBlocking = false, float $timeout = 0): void;
     public function send(string $queue, string $exchange, array $message): void;
     public function bind(string $queue, string $exchange);
     public function consume(string $queue, callable $handler);
