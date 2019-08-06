620fc1a237dcf6f3189dd24c202f2a123abb4ee0
ueef <artem@arkadev.org>
2019-08-06 11:07:00 +0500

    fix NullDriver

--- src/Drivers/NullDriver.php
+++ src/Drivers/NullDriver.php
@@ -7,7 +7,7 @@ use Ueef\Postbox\Interfaces\DriverInterface;
 
 class NullDriver implements DriverInterface
 {
-    public function wait(): void {}
+    public function wait(bool $nonBlocking = false, float $timeout = 0): void {}
     public function send(string $queue, string $exchange, string $message): void {}
     public function bind(string $queue, string $exchange) {}
     public function consume(string $queue, callable $callback): void {}
